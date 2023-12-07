63bfa4f1c7668ee64e603c2dae9726168ab0e2a3
Londiwetamia <Londiwetamia98@gmail.com>
2023-12-07 10:21:10 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,22 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+    size_t count = 0;
 
-	count = 0;
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        count++;
+    }
 
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		count++;
-		h = h->next;
-	}
-
-	return (count);
+    return count;
 }
